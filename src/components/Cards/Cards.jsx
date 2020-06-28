d5a8c9f05a5b09ab6c6e695246ba4effc3270f16
MuhammadHammadAhmed <hammad.tco@gmail.com>
2020-06-28 17:57:52 +0500

    all three template cards, rendered

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -15,6 +15,24 @@ const Cards=(props) =>{
 <Typography variant ="body2">The number of active Cases of COVID-19</Typography>
     </CardContent>
 </Grid>
+
+<Grid item component ={Card}>
+    <CardContent>
+<Typography color ="textSecondary" putterBottom>Recoveries</Typography>
+<Typography variant ="h5">REAL DATA</Typography>
+<Typography color ="textSecondary" >REAL DATE</Typography>
+<Typography variant ="body2">The number of recovered patientsfrom COVID-19</Typography>
+    </CardContent>
+</Grid>
+
+<Grid item component ={Card}>
+    <CardContent>
+<Typography color ="textSecondary" putterBottom>Deaths</Typography>
+<Typography variant ="h5">REAL DATA</Typography>
+<Typography color ="textSecondary" >REAL DATE</Typography>
+<Typography variant ="body2">The number of deats due to COVID-19</Typography>
+    </CardContent>
+</Grid>
 </Grid>
         </div>
         
