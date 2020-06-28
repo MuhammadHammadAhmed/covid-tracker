6f5841064779f2f10d3f1691186fb711260a247f
MuhammadHammadAhmed <hammad.tco@gmail.com>
2020-06-28 17:46:17 +0500

    rendered first sample card in Cards Component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,15 +5,16 @@ import  styles from './Cards.module.css';
 const Cards=(props) =>{
     console.log("CARDSPROP",props);
     return(
-        <div className= {styles.Container}>
+        <div className={styles.Container}>
 <Grid container spacing={3} justify='center'>
-    <Grid item component ={Card}></Grid>
+    <Grid item component ={Card}>
     <CardContent>
 <Typography color ="textSecondary" putterBottom>Infected</Typography>
 <Typography variant ="h5">REAL DATA</Typography>
 <Typography color ="textSecondary" >REAL DATE</Typography>
 <Typography variant ="body2">The number of active Cases of COVID-19</Typography>
     </CardContent>
+</Grid>
 </Grid>
         </div>
         
