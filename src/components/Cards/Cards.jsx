8cd184d1eca35380df350297c22ba70cb93cd524
MuhammadHammadAhmed <hammad.tco@gmail.com>
2020-07-01 10:17:16 +0500

    rendered the chart component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,7 +5,7 @@ import  styles from './Cards.module.css';
 import cx from 'classnames';
 
 const Cards=({data:{confirmed,deaths,lastUpdate,recovered}}) =>{
-    console.log("Confirmed",confirmed);
+    
     if(!confirmed){
         return ('Loading');
     }
--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,38 +1,50 @@
 import React ,{useEffect,useState} from 'react';
 import {fetchDailyData}from '../../api/';
-//import {Line, Bar} from 'react-chartjs-2';
+import {Line, Bar} from 'react-chartjs-2';
 
 const Charts=() =>{
-    const [dailyData,setDailyData]= useState({});
+    const [dailyData,setDailyData]= useState([]);
 
     useEffect(()=>{
 
         loadDailyData();
-
     })
     const loadDailyData =async()=>{
-    //    const data=await fetchDailyData();
+       const data=await fetchDailyData();
 
-     //   setDailyData(data);
-        console.log("DAILY DATA",dailyData);
+       setDailyData(data);
+       console.log("DAILY DATA",dailyData);
     }
-//    const lineChart= (
-//       dailyData[0]?(
-//         <Line
-//         data={{
-//     labels:'' ,
-//     datasets: [{},{}],
-// }}
+   const lineChart= (
+      dailyData.length?(
+        <Line
+        data={{
+    labels:dailyData.map(({date})=> date),
+    datasets: [{
+        data:dailyData.map(({confirmed})=> confirmed),
+        labels:'Infected',
+        bordercolor :'#3333ff',
+        fill: true
+    },{
+        data:dailyData.map(({deaths})=> deaths),
+        labels:'Infected',
+        bordercolor :'#red',
+        backgroundColor:'rgba(255,0,0,0.5)',
+        fill: true
+    }],
+}}
 
     
 
 
-// />
+/>
       
    
-// ): null);
+): null);
     return(
-        <h1>Charts</h1>
+        <div className={StyleSheet.container}>
+            {lineChart}
+        </div>
     
     )
 }
